981e23a9bbcda817dedf9a81db1f9c4ea9cb9386
Jack Shipway <jack.shipway@ascential.com>
2017-05-19 15:41:39 +0100

    Successfully retrieve data from source

--- src/analysis/build_features.py
+++
@@ -1,5 +0,0 @@
-import os
-from Code.config import get_path
-
-print get_path()
-
---
+++ src/analysis/cannes.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from Code.config import get_path
+
+PATH = get_path()
+
+
+
+data = pd.DataFrame(pd.read_csv(PATH+'/Cannes/Data/AllDelegateTrakScanners.csv'))
+
+
+
+
+
+
+

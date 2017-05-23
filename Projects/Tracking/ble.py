9cea6e659c79af001c14e204f32a66404cec6ef5
Jack Shipway <jack.shipway@ascential.com>
2017-05-23 11:21:41 +0100

    Groupby stage

--- Projects/Tracking/ble.py
+++ Projects/Tracking/ble.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import numpy as np
 from Code.config import get_path
-from clean import clean_ble
+from clean_functions import clean_ble, rssi_to_metres
 
 
 PATH = get_path()
+
+# Clean data
 ble = clean_ble(PATH, pd.DataFrame(pd.read_csv(PATH+'/Processed/ble_39184.txt', sep='\t')))
+# Compute distances (in metres) from sensor to beacon
+ble = pd.DataFrame(rssi_to_metres(ble))
+
+print ble
 
 
+
+# print ble.groupby(['datetime', 'sensor'])['id', 'power', 'rssi', 'accuracy',
+#                                           'id_sensor', 'id_location', 'metres'].first().reset_index()
 
 
--- Projects/Tracking/rssi_to_metres.py
+++
@@ -1,5 +0,0 @@
-
-
-def compute_dist(df):
-    df['metres'] = 10 ** ((df['power'] - df['rssi']) / 20.0)
-    return df

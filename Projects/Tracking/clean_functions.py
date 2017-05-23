e3d821e6acc6ed749802ab103e2b69efec18faec
Jack Shipway <jack.shipway@ascential.com>
2017-05-23 15:27:44 +0100

    Added pairs and triple calculations - returns np arrays

--- Projects/Tracking/clean_functions.py
+++ Projects/Tracking/clean_functions.py
@@ -1,12 +1,13 @@
 import pandas as pd
+import numpy as np
 
 
 def clean_ble(PATH, df):
 
     # Remove unwanted columns
-    df = df.drop(['clientmac', 'proximity', 'type', 'probetime_gmt', 'probetime'], axis=1)
+    df = df.drop(['clientmac', 'type', 'probetime_gmt', 'probetime'], axis=1)
     # Rename column headers
-    df.columns=['id', 'datetime', 'sensor', 'power', 'rssi', 'accuracy']
+    df.columns=['id', 'datetime', 'sensor', 'proximity', 'power', 'rssi', 'accuracy']
     # Merge with stand locations
     sensor_stand_loc = pd.merge(
         pd.DataFrame(pd.read_csv(PATH.replace('BLE/Data', 'Location/', 1) + '/stand_locations.txt',
@@ -28,10 +29,33 @@ def clean_ble(PATH, df):
     map_sensors = {sensor: i for i, sensor in enumerate(set(df['sensor']))}
     df['sensor'] = df['sensor'].map(map_sensors)
     # Map datetime strings to datetime - chop off seconds: eg. 12:00:15 => 12:00:00
-    df['datetime'] = pd.to_datetime(df['datetime']).map(lambda x: x.replace(second=0))
+    df['datetime'] = pd.to_datetime(df['datetime']) #.map(lambda x: x.replace(second=0))
     return df
 
 
 def rssi_to_metres(df):
     df['metres'] = 10 ** ((df['power'] - df['rssi']) / 20.0)
     return df
+
+
+def identify_pairs(df):
+    pairs = []
+    start_time, start_sensor = df['datetime'][0], df['sensor'][0]
+    for i in range(len(df)-1):
+        next_time, next_sensor = df['datetime'][i+1], df['sensor'][i+1]
+        if (next_time-start_time).seconds < 30:
+            if next_sensor != start_sensor:
+                pairs.append([0, start_time, start_sensor, df['proximity'][i], df['metres'][i]])
+                pairs.append([0, next_time, next_sensor, df['proximity'][i+1], df['metres'][i+1]])
+        start_time, start_sensor = next_time, next_sensor
+    return np.array(pairs)
+
+
+def identify_triples(pairs):
+    triples = []
+    for i in range(len(pairs)-3):
+        triple = pairs[i:i+4, :]
+        if (triple[3, 1] - triple[0, 1]).seconds < 30:
+            if len(pd.unique(triple[:, 2])) == 3:
+                triples.append(triple)
+    return np.array(triples)

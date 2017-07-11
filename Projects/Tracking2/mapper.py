cb94e9755c3138cec6a14339da584afba76949af
Jack Shipway <jack.shipway@ascential.com>
2017-07-11 15:50:27 +0100

    Changed tile size test

--- Projects/Tracking2/mapper.py
+++ Projects/Tracking2/mapper.py
@@ -13,24 +13,24 @@ def main():
 
     # Set arena size and tile size (granularity) manually
     print 'Constructing Arena...\n'
-    tile_size = 25
+    tile_size = 100
     x_min, x_max = 0, 3000
     y_min, y_max = 0, 1000
 
     # Construct event arena with variables above
     arena = ob.EventArena(x_min, x_max, y_min, y_max, tile_size)
     tiles = arena.enum_tiles()
-    # Map each square to a number from 0 to n
-    mapper = {i: (tiles[i].centre().x, tiles[i].centre().y) for i in range(len(tiles))}
+    # Enumerate tiles
+    enum_tiles = {i: (tiles[i].centre().x, tiles[i].centre().y) for i in range(len(tiles))}
 
-    for delegate in delegate_ids:
+    for delegate in delegate_ids[10:]:
 
         # Ignore syntax warning
         query = "SELECT * FROM bettshowexcel2017.logs_beacons_nikos WHERE type = 'ble' AND measuredpower < 0" \
                 " AND proximity = 'Near' AND uuidmm = ('%s')" % delegate
 
         # Create, analyse and plot an event map for each delegate
-        event_map(path, delegate, query, arena, mapper)
+        event_map(path, delegate, query, arena, enum_tiles)
 
 
 if __name__ == '__main__':
